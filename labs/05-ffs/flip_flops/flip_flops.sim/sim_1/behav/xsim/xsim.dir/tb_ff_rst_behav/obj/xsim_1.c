5300a328a734d038317aa2dfef393503db2e2164
Petr Vaněk <petrvaneek@gmail.com>
2022-05-01 13:36:59 +0200

    d

--- labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/obj/xsim_1.c
+++ labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/obj/xsim_1.c
@@ -67,8 +67,8 @@ const int NumRelocateId= 7;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/tb_ff_rst_behav/xsim.reloc",  (void **)funcTab, 7);
-	iki_vhdl_file_variable_register(dp + 3560);
-	iki_vhdl_file_variable_register(dp + 3616);
+	iki_vhdl_file_variable_register(dp + 3600);
+	iki_vhdl_file_variable_register(dp + 3656);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
